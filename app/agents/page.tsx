5e7ea0aeecfc448f5cb73b331824a76eb1502c8e
zepropre-stack <zepropre@gmail.com>
2026-06-26 10:16:40 +0200

    Add app/agents/page.tsx

---
+++ app/agents/page.tsx
@@ -0,0 +1,7 @@
+import { createAdminSupabase } from '@/lib/supabase-server'
+import AgentCard from '@/components/AgentCard'
+import AgentFilters from '@/components/AgentFilters'
+
+export default async function AgentsPage() {
+  return <div>Agents</div>
+}
